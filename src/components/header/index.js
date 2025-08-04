3d123ee0c67dae841e835eebc8b70d9d00a82e0f
mahesh <dev030.rejoice@gmail.com>
2025-08-04 14:23:09 +0530

    style:scroll direction styling change

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './header.module.scss';
 import Logo from '../logo';
 import UserIcon from '../icons/userIcon';
@@ -7,36 +7,68 @@ import MenuIcon from '../icons/menuIcon';
 import classNames from 'classnames';
 import CloseIcon from '../icons/closeIcon';
 import Link from 'next/link';
+function useScrollDirection() {
+  const [scrollDirection, setScrollDirection] = useState("noScroll");
+
+  useEffect(() => {
+    let lastScrollY = window.pageYOffset;
+    const updateScrollDirection = () => {
+      const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const scrollY = window.pageYOffset;
+      const direction = scrollY > lastScrollY ? "topToDown" : "downToTop";
+      if (currentScrollTop === 0) {
+        setScrollDirection("noScroll");
+      } else if (direction !== scrollDirection && (scrollY - lastScrollY > 10 || scrollY - lastScrollY < -10)) {
+        setScrollDirection(direction);
+      }
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+    };
+    window.addEventListener("scroll", updateScrollDirection);
+    return () => {
+      window.removeEventListener("scroll", updateScrollDirection);
+    };
+  }, [scrollDirection]);
+  return scrollDirection;
+}
 export default function Header() {
+  const scrollDirection = useScrollDirection();
   const [header, setHeader] = useState(false);
   return (
     <>
-    <header className={styles.header}>
-      <div className='container'>
-        <div className={styles.headerDesign}>
-          <Logo />
-          <div className={styles.rightAlignmet}>
-            <div className={styles.menu}>
-              <a className={styles.active} aria-label='Courses'>Courses</a>
-              <a aria-label='asasa'>Live Online Classes</a>
-              <a aria-label='Offline Sessions'>Offline Sessions</a>
-              <a aria-label='Community'>Community</a>
-              <a aria-label='Resources'>Resources</a>
-              <a aria-label='Blog'>Blog</a>
-              <a aria-label='About Us'>About Us</a>
-            </div>
-            <Link href="/login">
-            <div className={styles.profile}>
-              <UserIcon />
-            </div>
-            </Link>
-            <div className={classNames(styles.profile, styles.menuIcon)} onClick={() => setHeader(!header)}>
-              <MenuIcon />
+      <header
+        className={classNames(
+          styles.header,
+          scrollDirection === 'downToTop'
+            ? styles.show
+            : scrollDirection === 'noScroll'
+              ? null
+              : styles.hide
+        )}>
+        <div className='container'>
+          <div className={styles.headerDesign}>
+            <Logo />
+            <div className={styles.rightAlignmet}>
+              <div className={styles.menu}>
+                <a className={styles.active} aria-label='Courses'>Courses</a>
+                <a aria-label='asasa'>Live Online Classes</a>
+                <a aria-label='Offline Sessions'>Offline Sessions</a>
+                <a aria-label='Community'>Community</a>
+                <a aria-label='Resources'>Resources</a>
+                <a aria-label='Blog'>Blog</a>
+                <a aria-label='About Us'>About Us</a>
+              </div>
+              <Link href="/login">
+                <div className={styles.profile}>
+                  <UserIcon />
+                </div>
+              </Link>
+              <div className={classNames(styles.profile, styles.menuIcon)} onClick={() => setHeader(!header)}>
+                <MenuIcon />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
       <div className={classNames(styles.mobileHeader, header ? styles.show : styles.hide)}>
         <div className={styles.logoCloseIcon}>
           <Logo />
