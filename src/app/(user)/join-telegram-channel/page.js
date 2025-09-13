1a68d9faedb83dd7f1f4ad85362985965e63621f
Shreya Rejoice <shreya356.rejoice@gmail.com>
2025-09-13 11:08:32 +0530

    wrapped in suspense

--- src/app/(user)/join-telegram-channel/page.js
+++ src/app/(user)/join-telegram-channel/page.js
@@ -1,10 +1,12 @@
 import JoinTelegram from '@/modules/joinTelegram'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 export default function page() {
     return (
         <div>
-            <JoinTelegram />
+            <Suspense fallback={<div>Loading...</div>}>
+                <JoinTelegram />
+            </Suspense>
         </div>
     )
 }
--- src/app/(user)/our-course-details/page.js
+++ src/app/(user)/our-course-details/page.js
@@ -1,10 +1,12 @@
 import OurCourseDetails from '@/modules/ourCourseDetails'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 export default function page() {
     return (
         <div>
+            <Suspense fallback={<div>Loading...</div>}>
             <OurCourseDetails />
+            </Suspense>
         </div>
     )
 }
